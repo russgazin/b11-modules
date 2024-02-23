c459e6bed884c92bd6777c451c2b7130ba729282
russ gazin <russgazin@gmail.com>
2024-02-22 18:40:27 -0600

    subnet module built

---
+++ subnet_module/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  
+}
+
+variable "cidr_block" {
+  
+}
+
+variable "availability_zone" {
+  
+}
+
+variable "map_public_ip_on_launch" {
+  
+}
+
+variable "subnet_tag" {
+  
+}
