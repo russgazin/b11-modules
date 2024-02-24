8d756f37839fcedd904d69dd102cd1fee3492e0d
russ gazin <russgazin@gmail.com>
2024-02-23 20:45:36 -0600

    sg module built

--- sg_module/main.tf
+++ sg_module/main.tf
@@ -3,26 +3,19 @@ resource "aws_security_group" "sg" {
   description = var.description
   vpc_id      = var.vpc_id
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = var.sg_tag
   }
 }
 
 resource "aws_security_group_rule" "my_rule" {
-  description              = var.sg_description
-  type                     = var.type
-  from_port                = var.from_port
-  to_port                  = var.to_port
-  protocol                 = var.protocol
-  cidr_blocks              = length(var.cidr_blocks) <= 18 ? var.cidr_blocks : null
-  source_security_group_id = startswith(var.source_security_group_id, "sg-") ? var.source_security_group_id : null
+  for_each                 = var.sg_rules
+  description              = each.key
+  type                     = each.value[0]
+  from_port                = each.value[1]
+  to_port                  = each.value[2]
+  protocol                 = each.value[3]
+  cidr_blocks              = length(each.value[4]) <= 18 ? [each.value[4]] : null
+  source_security_group_id = startswith(each.value[4], "sg-") ? each.value[4] : null
   security_group_id        = aws_security_group.sg.id
 }
-
--- sg_module/variables.tf
+++ sg_module/variables.tf
@@ -14,30 +14,6 @@ variable "sg_tag" {
   
 }
 
-variable "sg_description" {
-  
-}
-
-variable "type" {
-  
-}
-
-variable "from_port" {
-  
-}
-
-variable "to_port" {
-  
-}
-
-variable "protocol" {
-  
-}
-
-variable "cidr_blocks" {
-  
-}
-
-variable "source_security_group_id" {
+variable "sg_rules" {
   
 }
